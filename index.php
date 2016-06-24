873ec721f0831f5ed1b8c64a2058fe6c5551f996
giovanebonifacio <bonifacio.gio@gmail.com>
2016-06-24 00:03:54 -0500

    1st Push

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<div class="header">
+	SafeChicago
+</div>
+
+<div class="graphic">
+  <h2>Graphic 1</h2>
+  <p>Lorem ipsum Lorem ipsum .</p>
+  <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum .</p>
+</div>
+
+<div class="graphic">
+  <h2>Graphic 2</h2>
+  <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum .</p> 
+  <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum .</p>
+</div>
+
+<div class="graphic">
+  <h2>Graphic 3</h2>
+  <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem.</p>
+  <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum .</p>
+</div>
+
+<div class="graphic">
+  <h2>Graphic 4</h2>
+  <p>Lorem ipsum Lorem ipsum Lorem ipsum.</p>
+  <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum .</p>
+</div>
+
+<div class="footer">
+	About Us
+</div>
+</body>
+</html>
